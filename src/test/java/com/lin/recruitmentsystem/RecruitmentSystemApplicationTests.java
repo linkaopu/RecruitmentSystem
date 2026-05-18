c0c70cf0109708288f2704f0222f6712eccaa1a6
林靠谱 <2144775778@qq.com>
2026-05-19 00:32:11 +0800

    first commit

---
+++ src/test/java/com/lin/recruitmentsystem/RecruitmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.recruitmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
